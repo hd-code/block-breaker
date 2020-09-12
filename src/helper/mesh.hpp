dffe0db5be832472dda0625474bbb1480ec04429
Hannes Dröse <hd@HDsMacBookPro.fritz.box>
2020-09-12 17:41:41 +0200

    tmp

---
+++ src/helper/mesh.hpp
@@ -0,0 +1,12 @@
+#pragma once
+
+#include "yoshix.h"
+
+// -----------------------------------------------------------------------------
+
+const int INDICES_PER_TRIANGLE = 3;
+
+gfx::BHandle CreateMesh(
+    int numOfVerts, float *vertices,
+    int numOfTriangles, int *triangles,
+    gfx::BHandle &material);
