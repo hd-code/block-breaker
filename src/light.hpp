28fa295cad5be7a1cebfab54737b560518293bf7
unknown <regfhd@gmail.com>
2020-09-13 15:47:20 +0200

    final setting of light

--- src/light.hpp
+++ src/light.hpp
@@ -5,6 +5,6 @@
 // The defaults are used to set the light settings. This is easier to read and
 // understand, in contrast to having a bunch of constants.
 struct SLight {
-    float direction[3] = { -1.0f, -0.7f, 4.0f };
+    float direction[3] = { 1.0f, -0.7f, 1.0f };
     float ambient[3] = { 0.2f, 0.2f, 0.2f };
 };
