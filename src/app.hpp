d6058f061d9baff930f8e6b8dc6bbedacec4f363
hd-code <54773293+hd-code@users.noreply.github.com>
2020-09-14 22:22:30 +0200

    Last fixes (#4)
    * minor fixes
    
    * fixed compiler warning
    
    Co-authored-by: Hannes Dröse <hd@HDsMacBookPro.local>

--- src/app.hpp
+++ src/app.hpp
@@ -5,57 +5,55 @@
 #include "key.hpp"
 #include "yoshix.h"
 
-#include <vector>
-
 // -----------------------------------------------------------------------------
 
 class CApplication : public gfx::IApplication {
-    public:
-        CApplication();
-        virtual ~CApplication();
+public:
+    CApplication();
+    virtual ~CApplication();
 
-    private:
-        // Textures
-        gfx::BHandle textures[int(ETexture::LENGTH)];
+private:
+    // Textures
+    gfx::BHandle textures[int(ETexture::LENGTH)];
 
-        // Constant Buffers
-        gfx::BHandle generalVSBuffer;
-        gfx::BHandle generalPSBuffer;
-        gfx::BHandle entityVSBuffer;
-        gfx::BHandle entityPSBuffer;
+    // Constant Buffers
+    gfx::BHandle generalVSBuffer;
+    gfx::BHandle generalPSBuffer;
+    gfx::BHandle entityVSBuffer;
+    gfx::BHandle entityPSBuffer;
 
-        // Shaders
-        gfx::BHandle vertexShader;
-        gfx::BHandle pixelShader;
+    // Shaders
+    gfx::BHandle vertexShader;
+    gfx::BHandle pixelShader;
 
-        // Materials
-        gfx::BHandle material;
+    // Materials
+    gfx::BHandle material;
 
-        // Meshes
-        gfx::BHandle ballMesh;
-        gfx::BHandle blockMesh;
-        gfx::BHandle dialogMesh;
-        gfx::BHandle paddleMesh;
+    // Meshes
+    gfx::BHandle ballMesh;
+    gfx::BHandle blockMesh;
+    gfx::BHandle dialogMesh;
+    gfx::BHandle paddleMesh;
 
-        // Game
-        CGame* game;
-        EKey key;
+    // Game
+    CGame* game;
+    EKey key;
 
-    private:
-        virtual bool InternOnStartup();
-        virtual bool InternOnShutdown();
-        virtual bool InternOnCreateTextures();
-        virtual bool InternOnReleaseTextures();
-        virtual bool InternOnCreateConstantBuffers();
-        virtual bool InternOnReleaseConstantBuffers();
-        virtual bool InternOnCreateShader();
-        virtual bool InternOnReleaseShader();
-        virtual bool InternOnCreateMaterials();
-        virtual bool InternOnReleaseMaterials();
-        virtual bool InternOnCreateMeshes();
-        virtual bool InternOnReleaseMeshes();
-        virtual bool InternOnResize(int width, int height);
-        virtual bool InternOnKeyEvent(unsigned int key, bool isDown, bool altDown);
-        virtual bool InternOnUpdate();
-        virtual bool InternOnFrame();
+private:
+    virtual bool InternOnStartup();
+    virtual bool InternOnShutdown();
+    virtual bool InternOnCreateTextures();
+    virtual bool InternOnReleaseTextures();
+    virtual bool InternOnCreateConstantBuffers();
+    virtual bool InternOnReleaseConstantBuffers();
+    virtual bool InternOnCreateShader();
+    virtual bool InternOnReleaseShader();
+    virtual bool InternOnCreateMaterials();
+    virtual bool InternOnReleaseMaterials();
+    virtual bool InternOnCreateMeshes();
+    virtual bool InternOnReleaseMeshes();
+    virtual bool InternOnResize(int width, int height);
+    virtual bool InternOnKeyEvent(unsigned int key, bool isDown, bool altDown);
+    virtual bool InternOnUpdate();
+    virtual bool InternOnFrame();
 };
